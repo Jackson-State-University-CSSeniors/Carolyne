0c0af62abd065290d8de85708acdfac527825f40
Carolyne Rutto <J00889934@students.jsums.edu>
2021-10-04 21:32:50 -0500

    test

--- Hello World project/Program.cs
+++ Hello World project/Program.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            //this is part of the assignment
+            
         }
     }
 }
